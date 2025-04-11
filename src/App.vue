ef5e83070ed7be89967d10cf24ae61de44879727
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-04-11 07:53:18 +0700

    Enchancement Navbar and Sidebar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import {ref,computed} from 'vue'
 
     import Navbar from "@/components/navbar/index.vue";
     import Footer from "@/components/footer/index.vue";
+    import Sidebar from "@/components/sidebar/index.vue"
     export default {
       name: "App",
       components: {
         Navbar,
         Footer,
+        Sidebar
       },
       data() {
         
@@ -21,10 +23,13 @@ import {ref,computed} from 'vue'
 <template>
   <main id="app" class="w-100 h-auto flex flex-col items-center">
     <Navbar />
-    <div class="w-[80%] h-auto mt-7 m-5">
+    <div class="w-full fixed h-auto top-[3.9rem] flex gap-10">
+      <div>
+        <Sidebar />
+      </div>
       <RouterView />
     </div>
-    <Footer />
+    <!-- <Footer /> -->
   </main>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,46 +5,6 @@ const routes = [
     path: "/",
     name: "home",
     component: () => import("@/views/home/index.vue"),
-  },
-  {
-    path: "/shop",
-    name: "shop",
-    component: () => import("@/views/shop/index.vue"),
-  },
-  {
-    path: "/products",
-    name: "products",
-    component: () => import("@/views/product/index.vue"),
-  },
-  {
-    path: "/contact",
-    name: "contact",
-    component: () => import("@/views/contact/index.vue"),
-  },
-  {
-    path: "/signup",
-    name: "signup",
-    component: () => import("@/views/SignUp/index.vue"),
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: () => import("@/views/SignIn/index.vue"),
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    component: () => import("@/views/profile/index.vue"),
-  },
-  {
-    path: "/view-cart",
-    name: "view-cart",
-    component: () => import("@/views/addToCart/index.vue"),
-  },
-  {
-    path: "/product/detail/:id",
-    name: "product-detail",
-    component: () => import("@/views/detail/index.vue")
   }
 ];
 
